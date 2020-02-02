25c378c1c89c427438d32d4a9632dffeb1843514
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-02-02 18:53:00 +0300

    task 9

--- flights/models.py
+++ flights/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_delete, post_delete
 
 class Flight(models.Model):
 	destination = models.CharField(max_length=100)
@@ -28,3 +29,26 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return str(self.user)
+
+@receiver(post_save, sender=User)
+def create_profile(instance, *args, **kwargs):
+	Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=Booking)   #instance is booking
+def adding_booked_miles(instance, *args, **kwargs):
+	user_miles = instance.user.profile.miles
+	flight_miles = instance.flight.miles
+	total_user_miles = user_miles + flight_miles
+	instance.user.profile.miles = total_user_miles
+	instance.user.profile.save()
+
+@receiver(post_delete, sender=Booking)   
+def deleting_booked_miles(instance, *args, **kwargs):
+	user_miles = instance.user.profile.miles
+	flight_miles = instance.flight.miles
+	total_user_miles = user_miles - flight_miles
+	instance.user.profile.miles = total_user_miles
+	instance.user.profile.save()
+
+
+
